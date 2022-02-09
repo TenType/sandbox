e0bc81c62c4823a0ad15bebd046e5db4aab4bb9b
TenType <55125103+TenType@users.noreply.github.com>
2022-02-08 21:32:31 -0800

    Updates

---
+++ index.0a03a7fc.js
@@ -0,0 +1,2 @@
+window.addEventListener("onclick",(()=>{alert("Hello world!")}));
+//# sourceMappingURL=index.0a03a7fc.js.map
---
+++ index.f4d7fca9.js
@@ -0,0 +1,2 @@
+window.addEventListener("click",(()=>{alert("Hello world!")}));
+//# sourceMappingURL=index.f4d7fca9.js.map
